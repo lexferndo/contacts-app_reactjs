8abe0e1cd48477f5a6ec547e308f3e99a74605d2
lexferndo <alexxfernandoo85@gmail.com>
2023-11-22 02:07:51 +0700

    Feature: React Context

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import PropTypes from "prop-types";
+import { LocaleConsumer } from "../contexts/LocaleContext";
 
 function SearchBar({ keyword, keywordChange }) {
   return (
-    <input
-      className="search-bar"
-      type="text"
-      placeholder="Cari berdasarkan nama"
-      value={keyword}
-      onChange={(event) => keywordChange(event.target.value)}
-    />
+    <LocaleConsumer>
+      {({ locale }) => {
+        return (
+          <input
+            className="search-bar"
+            type="text"
+            placeholder={
+              locale === "id" ? "Cari berdasarkan Nama" : "Search by Name"
+            }
+            value={keyword}
+            onChange={(event) => keywordChange(event.target.value)}
+          />
+        );
+      }}
+    </LocaleConsumer>
   );
 }
 
